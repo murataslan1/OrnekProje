b85ed47d871df4caac70a29ded07a2c0ae0a03b3
Murat Aslan <78961478+murataslan1@users.noreply.github.com>
2022-05-08 11:29:40 +0300

    Add project files.

---
+++ OrnekProje/Identity/IdentityInitializer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+
+namespace OrnekProje.Identity
+{
+    public class IdentityInitializer:CreateDatabaseIfNotExists<IdentityDataContext>
+    {
+        protected override void Seed(IdentityDataContext context)
+        {
+            if(!context.Roles.Any(i=>i.Name=="admin"))
+            {
+                var store = new RoleStore<ApplicationRole>(context);
+                var manager = new RoleManager<ApplicationRole>(store);
+                var role = new ApplicationRole() { Name = "admin", Description = "admin rolü" };
+                manager.Create(role);
+            }
+
+            if (!context.Roles.Any(i => i.Name == "user"))
+            {
+                var store = new RoleStore<ApplicationRole>(context);
+                var manager = new RoleManager<ApplicationRole>(store);
+                var role = new ApplicationRole() { Name = "user", Description = "user rolü" };
+                manager.Create(role);
+            }
+            if (!context.Users.Any(i => i.Name == "ilyasdagdelen"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+
+                var user = new ApplicationUser() { Name = "ilyas", Surname = "dagdelen", Email = "ilyasdagdelen@gmail.com", UserName = "ilyasdagdelen" };
+                manager.Create(user, "123456");
+                manager.AddToRole(user.Id, "admin");
+                manager.AddToRole(user.Id, "user");
+            }
+            if (!context.Users.Any(i => i.Name == "hamzadagdelen"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+
+                var user = new ApplicationUser() { Name = "hamza", Surname = "dagdelen", Email = "hamzadagdelen@gmail.com", UserName = "hamzadagdelen" };
+                manager.Create(user, "123456");
+               
+                manager.AddToRole(user.Id, "user");
+            }
+
+
+
+
+            base.Seed(context);
+        }
+    }
+}
